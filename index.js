c5fbf636d5c7b3592a9a518fb515359dac3198a6
JOÃO ALEXANDRE VIEIRA DE SOUZA <joao_av_souza@estudante.sesisenai.org.br>
2026-08-06 21:14:37 -0300

    mensagem

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import 'dotenv/config';
 import express from 'express'; 
 
 
-const port = process.env.PORT ;
+const port = process.env.PORT || 3000;
 const app = express();
 
 app.use(express.json());
@@ -10,11 +10,9 @@ app.use(express.json());
 app.get("/", (req, res) => {
     res.json({
         message: "Funcionando"
-    })
-})
+    });
+});
 
-app.listen(port)
-
-console.log("Backend rodando!")
-
-
+app.listen(port, () => {
+    console.log(`Backend rodando na porta ${port}!`);
+});
